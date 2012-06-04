824c20c002a2dae2df09d8169a80e4ce9ce8ce4d
Roo Reynolds <roo.reynolds@gmail.com>
2012-06-04 20:57:57 +0100

    added form support for /fade. I think there's probably a better way of doing optional params

--- orb-sinatra.rb
+++ orb-sinatra.rb
@@ -10,8 +10,15 @@ get '/' do
   "Hello. Try /fade/red or /fade/ff0000"
 end
 
-get '/fade/:name' do
-  name = params[:name]
+get '/fade/:colour' do
+  handle_fade(params[:colour], orb)
+end
+
+get '/fade' do
+  handle_fade(params[:colour], orb)
+end
+
+def handle_fade(name, orb)
   color = (Color::CSS[name])
   if (color)
     color = color.to_rgb
@@ -19,7 +26,7 @@ get '/fade/:name' do
     color = Color::RGB.from_html(name)
   end
   orb.fade_to(color.red, color.green, color.blue)
-  "#{name} (red: #{color.red} green: #{color.green} blue: #{color.blue})"
+  "<html><body style='background-color:#{"#%02x%02x%02x" % [color.red, color.green, color.blue]}'><form name='input' action='/fade' method='get'><input type='text' name='colour'></form><p/>#{name} (red: #{color.red} green: #{color.green} blue: #{color.blue})</body></html>"
 end
 
 get '/jump/:name' do
